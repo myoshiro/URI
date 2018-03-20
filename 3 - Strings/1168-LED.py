35bc368d5442af37c1eba3b59d3c27970131bac6
Marco <36777383+myoshiro@users.noreply.github.com>
2018-03-20 07:09:21 -0300

    Create 1168-LED.py

---
+++ 3 - Strings/1168-LED.py
@@ -0,0 +1,18 @@
+"""
+1168 - LED by myoshiro
+"""
+
+bag = [6,2,5,5,4,5,6,3,7,6,6]
+
+N = int(input())
+
+for i in range(N):
+  
+  leds=0
+  
+  line = input()
+  
+  for c in line:
+    leds += bag[int(c)]
+    
+  print(str(leds) + ' leds')
